4a6c2504305cc4ec32a179e295655c13804bfb30
Martin <tincho1597lopez@gmail.com>
2023-01-26 20:38:09 -0300

    Add Validation and I make the code more readable and more efficient

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static('public'))
--- public/postJob.js
+++ public/postJob.js
@@ -1,34 +1,16 @@
-
-/* let jsonLength;
-
-fetch('./Jobs.json')
-  .then(response => response.json())
-  .then(data => {
-    jsonLength = data.length;
-    console.log(`The length of the JSON file is: ${jsonLength}`);
-  })
-  .catch(error => {
-    console.error(`Error fetching JSON: ${error}`);
-  });
-
-console.log(jsonLength); */
-
-
 function modifyFormData(formData) {
 
     const max = formData.get('max_salary_input');   // String
     const min = formData.get('min_salary_input');   // String
-    const salary = '$' + min + ' - ' + '$' + max + ' per year';
+    let salary = '';
 
     // add a new key-value pair
+    salary = '$' + min + ' - ' + '$' + max + ' per year';
     formData.append("salary", salary);
-    //formData.append("id",jobs_counter());
 
-    //console.log(jobs_counter());
     // delete a key-value pair
     formData.delete("min_salary_input");
     formData.delete("max_salary_input");
-
 
     // modify an existing key-value pair
     const team_data = formData.get('team_size');
@@ -48,45 +30,70 @@ function modifyFormData(formData) {
 }
 
 
-
-// Test 
-
-
-
-
-
-
 const postButton = document.getElementById('postButton');
 postButton.addEventListener('click', function (event) {
     event.preventDefault();
     const formData = new FormData(document.querySelector('form'));
-    const data = modifyFormData(formData);
-    const body = {};
-    const id = data.get('id');
-    console.log(id);
+    const max = Number(formData.get('max_salary_input'));
+    const min = Number(formData.get('min_salary_input'));
 
-    for (let [key, value] of data.entries()) {
-        body[key] = value;
+    // Validation of inputs 
+    const min_range = 60000;
+    const max_range = 350000;
+    let filled = true;
+
+    function validateInputs(min_range, max_range, min, max) {
+        return (min < max) && validateRange(min_range, max_range, min, max);
     }
 
-    fetch('./Jobs.json', {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            alert("New Job posted!");
-            location.reload();
+    function validateRange(min_range, max_range, min, max) {
+        return (min >= min_range && max <= max_range);
+    }
+
+    if (!validateInputs(min_range, max_range, min, max)) {
+        alert('Wrong Salary input or incorrect format');
+    }
+
+    const inputs_required = ['title', 'company', 'description', 'location', 'min_salary_input', 'max_salary_input'
+        , 'skills_required', 'experience'];
+
+    // check if all required inputs are filled
+    if (!inputs_required.every(input => formData.get(input) !== '')) {
+        alert('Error there are inputs not filled or are incorrects');
+        filled = false;
+    }
+    // End of Validation 
+
+    // if all inputs are valid and filled
+    if (filled && validateInputs(min_range, max_range, min, max)) {
+        //modify form data
+        const data = modifyFormData(formData);
+        const body = {};
+
+        //convert formdata to json
+        for (let [key, value] of data.entries()) {
+            body[key] = value;
+        }
+
+        // post the data to the server
+        fetch('./Jobs.json', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
         })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-
-
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                alert("New Job posted!");
+                location.reload();
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
 });
+
 
 
